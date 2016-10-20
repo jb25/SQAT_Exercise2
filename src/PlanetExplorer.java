1462354b88153123ceb8d7887eb27f052e6559fb
somename <someemail>
2016-10-20 14:47:21 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,11 @@
 // Finish time:
 
 public class PlanetExplorer {
+	
+	
+	private int x;
+	private int y;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,8 +17,18 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		
+		this.x=x;
+		this.y=y;
+		
 	}
 	
+	public PlanetExplorer(int x, int y) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+	}
+
 	public String executeCommand(String command){
 		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
@@ -28,4 +43,23 @@ public class PlanetExplorer {
 		
 		return null;
 	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+	
 }
