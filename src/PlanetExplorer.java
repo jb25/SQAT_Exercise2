d623dac3fcb06b78890d1b534b84d54951238670
jon bilbao <astrainjb25@gmail.com>
2016-10-20 16:39:33 +0300

    last class

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -1,7 +1,7 @@
 
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
-// ID:
-// Finish time:
+//finish time: 16:37
+//id: 119
 
 public class PlanetExplorer {
 
