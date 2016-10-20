67502593466c84365eedc74eff59b4ced1f45daf
somename <someemail>
2016-10-20 14:58:06 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,26 +4,26 @@
 // Finish time:
 
 public class PlanetExplorer {
-	
-	
+
+
 	private int x;
 	private int y;
-	private String f;acing
-	
+	private String f;
+
 	public PlanetExplorer(int x, int y, String obstacles){
-	/*	x and y represent the size of the grid.
-	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
-	 *  
+		/*	x and y represent the size of the grid.
+		 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
+		 *  
 		Example use: For a 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
-		 
-	 */
-		
+
+		 */
+
 		this.x=x;
 		this.y=y;
-		
+
 	}
-	
+
 	public PlanetExplorer(int x, int y) {
 		// TODO Auto-generated constructor stub
 		this.x=x;
@@ -43,17 +43,18 @@ public class PlanetExplorer {
 		 * Example: 
 		 * The explorer is on a 100x100 grid at location (0, 0) and facing NORTH. 
 		 * The explorer is given the commands "ffrff" and should end up at (2, 2) facing East.
-		 
+
 		 * The return string is in the format: "(pos_x,pos_y,facing)(obs1_x,obs1_y)(obs2_x,obs2_y)..(obsN_x,obsN_y)" 
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
-		
-		
+
+
 		if (command=="") {
 			return pos;
 		}
-		
+		return pos;
+
 	}
 
 	public int getX() {
@@ -71,7 +72,7 @@ public class PlanetExplorer {
 	public void setY(int y) {
 		this.y = y;
 	}
-	
-	
-	
+
+
+
 }
