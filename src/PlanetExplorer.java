826dd011967d5f71edcf3c74f93b7936724e9e52
somename <someemail>
2016-10-20 15:53:38 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -55,16 +55,15 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
-System.out.println(getDir());
 		if (command.length() > 1) {
 			pos= this.executeCommand(command.substring(0, 1));
 			return this.executeCommand(command.substring(1, command.length()));
 
 		}else if (command=="") {
 		} else if (command.equals("r")) {
-			pos= pos.replace(pos.charAt(pos.length()-2), turnRight(pos.charAt(pos.length()-2)));
+			pos= pos.replace(pos.charAt(pos.length()-2), turnRight(getDir()));
 		}else if (command.equals("l")) {
-			pos=pos.replace(pos.charAt(pos.length()-2), turnLeft(pos.charAt(pos.length()-2)));
+			pos=pos.replace(pos.charAt(pos.length()-2), turnLeft(getDir()));
 		} if (command.equals("f")) {
 			dir=pos.charAt(pos.length()-2);
 			setPosY(posY+1);
